551e382a4e336a71894b8c0881a4a88ed1fa2b98
Jarodbf <71373525+Jarodbf@users.noreply.github.com>
2021-09-22 13:33:12 +0200

    Update random.c

--- random.c
+++ random.c
@@ -1,14 +1,27 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
+int tab[20] = {1};
+int tab2[2000] = {1};
+int j;
 srand(time(NULL));
-for(int i=0;i<20;i++)
+for(int i=0;i<19;i++)
+    {
+
+    for(j=0;j<1999;j++)
+    {
+        while(tab2[j] > 45 || tab2[j] < 20)
         {
-        int r = ((rand()%16)+25);
-        printf("%d\n",r);
+        tab2[j] = (rand()%50 )+1;
         }
+    }
+        sleep(0.1);
+        tab[i] = tab2[i*99];
+        printf("%d\n", tab[i]);
+    }
 return 0;
 }
