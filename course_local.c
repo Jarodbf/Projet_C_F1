41be50666e866828797dff26e37c7a0cbb2c8b4f
raphaelm667 <71373096+raphaelm667@users.noreply.github.com>
2021-09-22 12:32:44 +0200

    Update course_local.c

--- course_local.c
+++ course_local.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 struct F1 {
+	int id;
         int temp[4];
         char statut; //S=stand O=out E=enCourse
 }
@@ -23,9 +24,11 @@ F44.temp[1] = généTemp();
 F44.temp[2] = généTemp();
 F44.temp[3] = F44.temp[0]+F44.temp[1]+F44.temp[2];
 	
-struct F1 voiture;
-voiture["F44"].temp[0] = généTemp();
-voiture["F44"].temp[1] = généTemp();
-voiture["F44"].temp[2] = généTemp();
-voiture["F44"].temp[3] = voiture[F44].temp[0]+voiture[F44].temp[1]+voiture[F44].temp[2];
+struct F1 struc_voiture;
+struc_voiture voiture[20];
+voiture[0].id = "F44";
+voiture[0].temp[0] = généTemp();
+voiture[0].temp[1] = généTemp();
+voiture[0].temp[2] = généTemp();
+voiture[0].temp[3] = voiture[0].temp[0]+voiture[0].temp[1]+voiture[0].temp[2];
 }
