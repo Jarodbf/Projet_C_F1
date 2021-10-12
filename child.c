b918c7a4eed0f733f2cb3cd455e3a148abae0e5f
Jarodbf <71373525+Jarodbf@users.noreply.github.com>
2021-10-12 14:01:23 +0200

    Update child.c
    oui bah pour un fils du coup et ça marche

--- child.c
+++ child.c
@@ -26,14 +26,14 @@ int * childRand()
             for(int j=0;j<3;j++)
             {
             tab[j]= geneTemp();
-            printf("child = %d avec le pid %d\n", tab[j], getpid());
+            //printf("child = %d avec le pid %d\n", tab[j], getpid());
             	
-            //int affichage = 12;
+        
             
             }
             write(p[1],&tab,sizeof(tab));
             dup2(p[1],1); // envoie le résultat au père
-            //close(p[1]);
+            close(p[1]);
             //exit(0);
             return tab;
 }
@@ -45,8 +45,8 @@ int main()
 pipe(p);
 
 srand(time(NULL));
-    for(int i=0;i<2;i++) // loop will run n times (n=5)
-    {
+    //for(int i=0;i<2;i++) // loop will run n times (n=5)
+    //{
         if(fork() == 0)
         {
             //int tab[3];
@@ -71,25 +71,26 @@ srand(time(NULL));
           //  {
                 wait(NULL);
                 int tab2[3];
+                read(p[0],tab2,sizeof(tab2));
                 //voiture[i].temp[j] = read(p[0],&voiture[i].temp[j],sizeof(voiture[i].temp[j]));
                 for(int j=0;j<3;j++)
                 {
-                read(p[0],tab2,sizeof(tab2));
                 
-                voiture[i].temp[j] = tab2[j];
-                printf("parent = %d\n", voiture[i].temp[j]);
+                
+                voiture[0].temp[j] = tab2[j];
+                //printf("parent = %d\n", voiture[0].temp[j]);
                 }
                 //printf("parent = %d\n", voiture[i].temp[j]);
-                //close(p[0]);
+                close(p[0]);
             //}
         }
-    }
+    //}
 
 strcpy(voiture[0].id,"F44");
 voiture[0].temp[3] = voiture[0].temp[0]+voiture[0].temp[1]+voiture[0].temp[2];
 printf("%s \n%d %d %d %d\n",voiture[0].id,voiture[0].temp[0],voiture[0].temp[1],voiture[0].temp[2],voiture[0].temp[3]);
-strcpy(voiture[1].id,"F45");
-voiture[1].temp[3] = voiture[1].temp[0]+voiture[1].temp[1]+voiture[1].temp[2];
-printf("%s \n%d %d %d %d\n",voiture[1].id,voiture[1].temp[0],voiture[1].temp[1],voiture[1].temp[2],voiture[1].temp[3]);
+//strcpy(voiture[1].id,"F45");
+//voiture[1].temp[3] = voiture[1].temp[0]+voiture[1].temp[1]+voiture[1].temp[2];
+//printf("%s \n%d %d %d %d\n",voiture[1].id,voiture[1].temp[0],voiture[1].temp[1],voiture[1].temp[2],voiture[1].temp[3]);
 
 }
