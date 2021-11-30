c61b609bb74ee0436fabf0607ff1cfc6fcf2e748
raphaelm667 <71373096+raphaelm667@users.noreply.github.com>
2021-11-30 16:18:55 +0100

    Add files via upload

--- propre/course.c
+++ propre/course.c
@@ -24,9 +24,9 @@ int geneTemp (int x){
 
 void afficheTab() {
 printf(" IDV |  BS1   |  BS2   |  BS3   |  BT   |\n\n");
-for(int k=0;k<V+5;k++)//voiture
+for(int k=0;k<V;k++)//voiture
     {
-       printf(" F%2d | %2d | %2d | %2d\n",voitures[1].id,voitures[1].temp[0],voitures[1].temp[1],voitures[1].temp[2]);
+       printf("F%2d | %2d | %2d | %2d\n",voitures[k].id,voitures[k].temp[0],voitures[k].temp[1],voitures[k].temp[2]);
     }
 	printf("--------------------------------------------------------------\n");
 	//printf(" F%2d | %3.2fs | F%2d | %3.2fs | F%2d | %3.2fs |\n",Bid[0],voiture[0].BStemp[0],Bid[1],voiture[0].BStemp[1],Bid[2],voiture[0].BStemp[2]);
@@ -37,7 +37,6 @@ int main()
 {
 srand(time(NULL));
 
-int vNum = 0;
 int pid;
 int NumVoit[20] = {44, 77, 11, 33, 3, 4, 5, 18, 14, 31, 16, 55, 10, 22, 7, 99, 9, 47, 6, 63};
 //for(int i=0;i<20;i++){
@@ -49,18 +48,17 @@ for(int k = 0; k<20 ; k++)
 	voitures[k].id = NumVoit[k]; 
   if ((pid = fork()) == 0)
   { 
-    voitures[1].temp[0] = geneTemp(getpid());
-	voitures[1].temp[1] = geneTemp(getpid());
-	voitures[1].temp[2] = geneTemp(getpid());
+    voitures[k].temp[0] = geneTemp(getpid());
+	voitures[k].temp[1] = geneTemp(getpid());
+	voitures[k].temp[2] = geneTemp(getpid());
     exit(1);
   }
   
     else{
     wait(NULL);// passe au père
 	//printf("%d\n",voitures[1].temp[0]);
-	 printf(" F%2d | %2d | %2d | %2d\n",voitures[1].id,voitures[1].temp[0],voitures[1].temp[1],voitures[1].temp[2]);	
+	 //printf("Pere F%2d | %2d | %2d | %2d\n",voitures[1].id,voitures[1].temp[0],voitures[1].temp[1],voitures[1].temp[2]);	
 	 //afficheTab();
-    vNum++;//le conteur des voitures
     
     }
   }
