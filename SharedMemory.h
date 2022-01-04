8804f39b57779c820efbb77449afc67aab2b664b
Jarodbf <71373525+Jarodbf@users.noreply.github.com>
2022-01-04 20:56:39 +0100

    Sémaphore
    Ajout du sémaphore dans le programme.

--- SharedMemory.h
+++ SharedMemory.h
@@ -3,21 +3,21 @@ struct F1 {
 	int idBst[4];
 	int temp[5];
 	int BStemp[4];
-    float timerCourse;
 	char statut; //S=stand O=out E=enCourse
+	int lost;
 };
 
 struct F1 * voitures;
 int shmid;
 int key=22;
 
-int connectShm(){
+void connectShm(){
 	
-	shmid = shmget(key,sizeof(struct F1)*20,IPC_CREAT|0666);
+	shmid = shmget(key,sizeof(struct F1)*21,IPC_CREAT|0666);
 	voitures = (struct F1*) shmat(shmid,0,0);
 }
 
-int disconectShm(){
+void disconectShm(){
 	shmdt(voitures);
 	shmctl(shmid, IPC_RMID, NULL);
 }
--- main.c
+++ main.c
@@ -11,9 +11,12 @@
 #include <time.h>
 #include "SharedMemory.h"
 #include <fcntl.h>
+#include <semaphore.h>
 
-# define V  21
+int V = 21;
 # define T  5
+sem_t s;
+
 struct F1 voitcpy[21];
 struct F1 classment[21];
 
@@ -25,11 +28,11 @@ int geneTemp (int x){
 
 void afficheTab() {
 	int kTemp;
-	system("clear");
-printf("   IDV   |  S1   |  S2   |  S3   |   TEMP TOT    |   BTEMP   |   ECART   |\n\n");
+//	system("clear");
+printf("   IDV   |  S1   |  S2   |  S3   |   TEMP TOT    |   BTEMP   |   ECART   |   STATUS   |\n\n");
 memcpy(&classment,voitcpy,sizeof(struct F1)*21);
-for (int i=0; i<20;i++){
-	for (int y=i+1; y<20;y++){
+for (int i=0; i<V-1;i++){
+	for (int y=i+1; y<V-1;y++){
 		if(classment[i].temp[4]>classment[y].temp[4])
 		{
 			classment[20] = classment[i];
@@ -41,17 +44,20 @@ for (int i=0; i<20;i++){
 
 for(int k=0;k<V-1;k++)//voiture
     {
-       printf("   F%2d   |   %2d   |   %2d   |   %2d   |   %2ds   |   %2ds   |   ",classment[k].id,classment[k].temp[0],classment[k].temp[1],classment[k].temp[2],classment[k].temp[3],classment[k].temp[4]);
-	   if(k == 0)
-	   {
-		   printf("0s\n");
-	   }
-	   else
-	   {
-		   kTemp = k-1;
-		   
-		   printf("%2ds\n",classment[k].temp[4] - classment[kTemp].temp[4]);
-	   }
+		if (classment[k].lost == 0){
+			
+		   printf("   F%2d   |   %2d   |   %2d   |   %2d   |   %2ds   |   %2ds   | %c  ",classment[k].id,classment[k].temp[0],classment[k].temp[1],classment[k].temp[2],classment[k].temp[3],classment[k].temp[4],classment[k].statut);
+		   if(k == 0)
+		   {
+			   printf("0s\n");
+		   }
+		   else
+		   {
+			   kTemp = k-1;
+			   
+			   printf("%2ds\n",classment[k].temp[4] - classment[kTemp].temp[4]);
+		   }
+		}
     }
 	printf(" Best S1 par F%2d: %2d\n",voitcpy[20].idBst[0], voitcpy[20].BStemp[0]);
 	printf(" Best S2 par F%2d: %2d\n",voitcpy[20].idBst[1], voitcpy[20].BStemp[1]);
@@ -60,113 +66,193 @@ for(int k=0;k<V-1;k++)//voiture
 	printf("--------------------------------------------------------------\n");
 
 }
-void WriteResult(){
-	int kTemp;
-	FILE * fp;
-	fp = fopen ("result.txt", "w");
-    if(fp < 0){ // vérifie que le fichier se soit ouvert
+void Ecrit(char *tour){
+	char buff[1024];
+	int f;
+	if(strcmp(tour , "P1")==0){
+    f = open("P1.txt", O_WRONLY); 
+	}
+	else if(strcmp(tour , "P2")==0){
+    f = open("P2.txt", O_WRONLY); 
+	}
+	else if(strcmp(tour , "P3")==0){
+    f = open("P3.txt", O_WRONLY); 
+	}
+	else if(strcmp(tour ,"Q1")==0){
+    f = open("Q1.txt", O_WRONLY); 
+	}
+	else if(strcmp(tour , "Q2")==0){
+    f = open("Q2.txt", O_WRONLY); 
+	}
+	else if(strcmp(tour , "Q3")==0){
+    f = open("Q3.txt", O_WRONLY); 
+	}
+    if(f < 0){ // vérifie que le fichier se soit ouvert
         perror("file ");
         exit(0);
     }
-	char ou[20] ;
-	for(int k=0;k<V-1;k++)//voiture
+    for(int k=0;k<V-1;k++)//voiture
     {
-	   fprintf(fp,"%d\n", classment[k].id);
-	   fprintf(fp,"%d\n", classment[k].temp[0]);
-	   fprintf(fp,"%d\n", classment[k].temp[1]);
-	   fprintf(fp,"%d\n", classment[k].temp[2]);
-	   fprintf(fp,"%d\n", classment[k].temp[3]);
-	   fprintf(fp,"%d\n", classment[k].temp[4]);
-
-	   if(k == 0)
-	   {
-		   fprintf(fp,"0s\n");
-	   }
-	   else
-	   {
-		   kTemp = k-1;
-		   
-		   int tempTemps = classment[k].temp[4] - classment[kTemp].temp[4];
-		   fprintf(fp,"%d\n", tempTemps);
-
-	   }
-    }
-	fprintf(fp,"%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", voitcpy[20].idBst[0], voitcpy[20].BStemp[0], voitcpy[20].idBst[1],voitcpy[20].BStemp[1],voitcpy[20].idBst[2], voitcpy[20].BStemp[2], voitcpy[20].idBst[3], voitcpy[20].BStemp[3]);
-	fclose(fp);
+       sprintf(buff,"%d\n",classment[k].id);
+       write(f, buff, strlen(buff));
+	}
+	close(f);
 }
-
-
-int main()
+int main(int argc,char *argv[])
 {
-srand(time(NULL));
-
-int pid;
-int NumVoit[20] = {44, 77, 11, 33, 3, 4, 5, 18, 14, 31, 16, 55, 10, 22, 7, 99, 9, 47, 6, 63};
-connectShm();
-for(int i=0;i<4;i++){
-voitures[20].BStemp[i] = 999;
-}
-struct timeval tempInitial , tempFinal;
-     int seconde = 0;
-     while(seconde < 1){
-		gettimeofday(&tempInitial , NULL);
-		//sleep(1);
-		for(int k = 0; k<20 ; k++)
+	sem_init(&s, 1, 1);
+	int oui = 56;
+	srand(time(NULL));
+	if (argc > 1)
+	{
+		int pid; 
+		int NumVoit[20] = {44, 77, 11, 33, 3, 4, 5, 18, 14, 31, 16, 55, 10, 22, 7, 99, 9, 47, 6, 63}; 
+		connectShm();
+		for(int k = 0; k<V-1 ; k++)
 		{
-			voitures[k].id = NumVoit[k]; 
-		if ((pid = fork()) == 0)
-		{ 
-			voitures[k].temp[0] = geneTemp(getpid());
-			
-			voitures[k].temp[1] = geneTemp(getpid());
-			
-			voitures[k].temp[2] = geneTemp(getpid());
-			
-			voitures[k].temp[3] = voitures[k].temp[0] + voitures[k].temp[1] +voitures[k].temp[2];
-			
-			voitures[k].temp[4] = voitures[k].temp[0] + voitures[k].temp[1] +voitures[k].temp[2];
-			if(voitures[k].temp[4] > voitcpy[k].temp[4] && voitcpy[k].temp[4] != 0)
-			{
-				voitures[k].temp[4] = voitcpy[k].temp[4];
-			}
-
-			if(voitures[20].BStemp[0]>voitures[k].temp[0])
-			{
-				voitures[20].BStemp[0] = voitures[k].temp[0];
-				voitures[20].idBst[0] = voitures[k].id;
-			}
-			if(voitures[20].BStemp[1]>voitures[k].temp[1])
-			{
-				voitures[20].BStemp[1] = voitures[k].temp[1];
-				voitures[20].idBst[1] = voitures[k].id;
-			}
-			if(voitures[20].BStemp[2]>voitures[k].temp[2])
-			{
-				voitures[20].BStemp[2] = voitures[k].temp[2];
-				voitures[20].idBst[2] = voitures[k].id;
-			}
-			if(voitures[20].BStemp[3]>voitures[k].temp[3])
-			{
-				voitures[20].BStemp[3] = voitures[k].temp[3];
-				voitures[20].idBst[3] = voitures[k].id;
-			}
-			
-			exit(1);
+			voitures[k].lost = 0;
+			voitures[k].id = NumVoit[k];
 		}
 		
+		if (strcmp(argv[1],"Q2")==0 || strcmp(argv[1],"Q3")==0)
+		{
+			int fichier =0;
+			if (strcmp(argv[1],"Q2")==0){
+				V = V-5;
+				fichier = open("Q1.txt", O_RDONLY );
+				for(int k = 15; k<20 ; k++)
+				{
+					voitures[k].lost = 1;
+				}
+			}
 			else{
-			wait(NULL);// passe au père
-			memcpy(&voitcpy,voitures,sizeof(struct F1)*21);
+				V = V-10;
+				fichier = open("Q2.txt", O_RDONLY );
+				for(int k = 10; k<20 ; k++)
+				{
+					voitures[k].lost = 1;
+				}
+			}
+			char* str = malloc(sizeof(char)*50);
+			int red;
+			char *classement;
+			red = read(fichier, str, sizeof(str)*30); 
+			close(fichier);
+			int pos[20];
+			int i=0;
+			classement = strtok(str,"\n");
+			while(classement!=NULL){
+				pos[i] = atoi(classement);
+				classement = strtok(NULL,"\n"); // prochaine phrase si présente
+				i++;
+			}
+			for(int k = 0; k<V-1 ; k++)
+			{
+			voitures[k].id = pos[k];
 			}
 		}
-		gettimeofday(&tempFinal , NULL);
-     seconde += (tempFinal.tv_sec - tempInitial.tv_sec);
-     
-		afficheTab();
-		printf("\nTotal time taken is : %d seconds and %lu microseconds\n",(seconde),(tempFinal.tv_usec - tempInitial.tv_usec));
-}
-disconectShm();
- //afiche le tableau des données 
- WriteResult();
+		for(int i=0;i<4;i++){
+		voitures[20].BStemp[i] = 999;
+		}
+			for(int k = 0; k<V-1 ; k++)
+				{
+				voitures[k].statut = 'E';
+				}
+		struct timeval tempInitial , tempFinal;
+			int seconde = 0;
+			while(seconde < 12){
+				gettimeofday(&tempInitial , NULL);
+				sleep(1);
+				for(int k = 0; k<V-1 ; k++)
+				{
+					if (voitures[k].lost == 0){
+						if ((pid = fork()) == 0)
+						{
+							//printf("\n%d\n", oui);
+
+							sem_wait(&s);
+							
+							if (voitures[k].statut != 'O'){
+								if (voitures[k].statut == 'S'){
+									voitures[k].statut = 'E';
+								}
+								if (geneTemp(getpid()) == 39){
+									voitures[k].statut = 'S';
+								}
+								if (geneTemp(getpid())+geneTemp(getpid())+geneTemp(getpid())+geneTemp(getpid()) == 143){
+									voitures[k].statut = 'O';
+								}
+								if (voitures[k].statut == 'E'){
+								voitures[k].temp[0] = geneTemp(getpid());
+								
+								voitures[k].temp[1] = geneTemp(getpid());
+								
+								voitures[k].temp[2] = geneTemp(getpid());
+								
+								voitures[k].temp[3] = voitures[k].temp[0] + voitures[k].temp[1] +voitures[k].temp[2];
+								
+								voitures[k].temp[4] = voitures[k].temp[0] + voitures[k].temp[1] +voitures[k].temp[2];
+								if(voitures[k].temp[4] > voitcpy[k].temp[4] && voitcpy[k].temp[4] != 0)
+								{
+									voitures[k].temp[4] = voitcpy[k].temp[4];
+								}
+
+								if(voitures[20].BStemp[0]>voitures[k].temp[0])
+								{
+									voitures[20].BStemp[0] = voitures[k].temp[0];
+									voitures[20].idBst[0] = voitures[k].id;
+								}
+								if(voitures[20].BStemp[1]>voitures[k].temp[1])
+								{
+									voitures[20].BStemp[1] = voitures[k].temp[1];
+									voitures[20].idBst[1] = voitures[k].id;
+								}
+								if(voitures[20].BStemp[2]>voitures[k].temp[2])
+								{
+									voitures[20].BStemp[2] = voitures[k].temp[2];
+									voitures[20].idBst[2] = voitures[k].id;
+								}
+								if(voitures[20].BStemp[3]>voitures[k].temp[3])
+								{
+									voitures[20].BStemp[3] = voitures[k].temp[3];
+									voitures[20].idBst[3] = voitures[k].id;
+								}
+								}
+							}
+							//printf("child");
+							sem_getvalue(&s, &oui);
+							sem_post(&s);
+														
+
+							exit(1);
+						}
+					
+						
+						
+						
+					}
+					
+				}
+				//printf("parent");
+						if(sem_wait(&s) == 0){
+							
+							//printf("\n%d\n", oui);
+							}
+						memcpy(&voitcpy,voitures,sizeof(struct F1)*21);
+						sem_post(&s);
+				gettimeofday(&tempFinal , NULL);
+			seconde += (tempFinal.tv_sec - tempInitial.tv_sec);
+			
+				afficheTab();
+				Ecrit(argv[1]);
+				//printf("\nTotal time taken is : %d seconds and %lu microseconds\n",(seconde),(tempFinal.tv_usec - tempInitial.tv_usec));
+		}
+		disconectShm();
+
+	}
+ //afiche le tableau des données
+ sem_destroy(&s);
 return 0;
 }
+
+
